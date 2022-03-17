3c878acfd6895767496966dd16f94117e258f014
paul-reiners <paul.reiners@gmail.com>
2022-03-17 12:24:23 -0500

    Got this finally running.

--- test/dcan/ScoreTest.py
+++ test/dcan/ScoreTest.py
@@ -1,9 +1,9 @@
-from unittest import TestCase
+import unittest
 
 from dcan.Score import do_total_scoring
 
 
-class ScoreTest(TestCase):
+class ScoreTest(unittest.TestCase):
     def test_do_total_scoring(self):
         parents_score_file = "data/sample/inputdata_conners3parent.csv"
         actual_results = do_total_scoring(parents_score_file)
@@ -12,3 +12,6 @@ class ScoreTest(TestCase):
         self.assertEqual(len(expected_keys), len(actual_results.keys()))
         for key in expected_keys:
             self.assertEqual(expected_results[key], actual_results[key])
+
+if __name__ == '__main__':
+    unittest.main()
