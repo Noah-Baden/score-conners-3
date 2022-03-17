67bf91f169bcf2e1368eada32dbcd95d3f19ad44
paul-reiners <paul.reiners@gmail.com>
2022-03-17 16:26:44 -0500

    Female/10

--- test/dcan/ScoreTest.py
+++ test/dcan/ScoreTest.py
@@ -13,5 +13,17 @@ class ScoreTest(unittest.TestCase):
         for key in expected_keys:
             self.assertEqual(expected_results[key], actual_results[key])
 
+
+    def test_do_total_scoring_female_10(self):
+        parents_score_file = "data/sample/inputdata_Conners3parent_female10.csv"
+        actual_results = do_total_scoring(parents_score_file, 10, "female")
+        expected_results = {'LP': (10, 64), 'PR': (10, 90), 'IN': (21, 82), 'AG': (9, 81), 'HY': (12, 67), 'EF': (21, 90)}
+        # IN (21, 82), HY (12, 67), LP (10, 64), EF (21, 90), AG (9, 81), PR (10, 90)
+        expected_keys = expected_results.keys()
+        self.assertEqual(len(expected_keys), len(actual_results.keys()))
+        for key in expected_keys:
+            self.assertEqual(expected_results[key], actual_results[key])
+
+
 if __name__ == '__main__':
     unittest.main()
