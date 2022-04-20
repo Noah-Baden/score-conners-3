72f0bf03b0d0ec7229cb6f94e42091e87f1e22ba
Noah-Baden <66318758+Noah-Baden@users.noreply.github.com>
2022-04-20 14:25:33 -0500

    Update score-conners-3.sh

--- score-conners-3.sh
+++ score-conners-3.sh
@@ -2,4 +2,4 @@ source /home/faird/shared/code/external/envs/miniconda3/load_miniconda3.sh
 conda activate nibabel_env
 export PYTHONPATH=$PYTHONPATH:./src
 
-python ./src/main.py --parents_file_name $1 --age $2 --sex $3
+python ./src/main.py --parents_file_name $1 --age $2 --sex $3 --parent_or_teacher
