6747c869e05f8f1aef7a906d297671d4436b8793
paul-reiners <paul.reiners@gmail.com>
2022-04-20 15:14:23 -0500

    Reformatting.

--- src/dcan/Score.py
+++ src/dcan/Score.py
@@ -1,26 +1,23 @@
 import math
-import sys
-from os.path import exists
 from collections import Counter
+from os.path import exists
 
 import pandas as pd
-import argparse
-from os.path import exists
 
 QUESTION_OFFSET = 5
 
 
-def do_total_scoring(parents_score_file, age, sex):
+def do_total_scoring(parents_score_file, age, sex, parents_or_teacher):
     # TODO Implement using teacher_score_file
-    lookup_table_file = "data/constant/scoringsheet_conners3parent.csv"
+    lookup_table_file = f"data/constant/scoringsheet_conners3{parents_or_teacher}.csv"
     column_name_to_score = do_scoring(parents_score_file, lookup_table_file)
-    t_score = get_t_score(age, sex, column_name_to_score)
+    t_score = get_t_score(age, sex, column_name_to_score, parents_or_teacher)
 
     return t_score
 
 
-def get_area_scores(question_number, parents_scores_df, lookup_df):
-    score = parents_scores_df.iloc[0][question_number]
+def get_area_scores(question_number, scores_df, lookup_df):
+    score = scores_df.iloc[0][question_number]
     looked_up_score = lookup_df.iloc[question_number][score + 1]
     area_name_to_score = {}
     for area_col in range(QUESTION_OFFSET, 8):
@@ -33,16 +30,16 @@ def get_area_scores(question_number, parents_scores_df, lookup_df):
 
 
 # TODO Implement for teacher_score_file
-def do_scoring(parents_scores_file, lookup_table_file):
-    parents_scores_df = pd.read_csv(parents_scores_file)
-    parents_scores_df = parents_scores_df.iloc[:, QUESTION_OFFSET:]
-    question_count = parents_scores_df.size
+def do_scoring(scores_file, lookup_table_file):
+    scores_df = pd.read_csv(scores_file)
+    scores_df = scores_df.iloc[:, QUESTION_OFFSET:]
+    question_count = scores_df.size
     area_name_to_score = {}
     lookup_df = pd.read_csv(lookup_table_file)
     lookup_df.fillna('', inplace=True)
 
     area_scores = \
-        [get_area_scores(question_number, parents_scores_df, lookup_df) for question_number in range(question_count)]
+        [get_area_scores(question_number, scores_df, lookup_df) for question_number in range(question_count)]
     for area_score in area_scores:
         c_dict = Counter(area_name_to_score) + Counter(area_score)
         area_name_to_score = c_dict
@@ -55,10 +52,10 @@ def do_scoring(parents_scores_file, lookup_table_file):
     return area_name_to_score
 
 
-def get_t_score(age, gender, column_name_to_score):
+def get_t_score(age, gender, column_name_to_score, parents_or_teacher):
     result = {}
     for key in column_name_to_score.keys():
-        csv_file = f'data/constant/{gender}_{key.lower()}.csv'
+        csv_file = f'data/constant/{parents_or_teacher}/{gender}_{key.lower()}.csv'
         if not exists(csv_file):
             continue
         df = pd.read_csv(csv_file)
--- test/dcan/ScoreTest.py
+++ test/dcan/ScoreTest.py
@@ -7,7 +7,7 @@ from dcan.Score import do_total_scoring, get_t_score_from_raw_score
 class ScoreTest(unittest.TestCase):
     def test_do_total_scoring(self):
         parents_score_file = "data/sample/inputdata_conners3parent.csv"
-        actual_results = do_total_scoring(parents_score_file, 9, "male")
+        actual_results = do_total_scoring(parents_score_file, 9, "male", 'parent')
         expected_results = {'LP': (7, 57), 'PR': (0, 44), 'IN': (4, 45), 'AG': (0, 43), 'HY': (1, 40), 'EF': (6, 48)}
         expected_keys = expected_results.keys()
         self.assertEqual(len(expected_keys), len(actual_results.keys()))
@@ -16,7 +16,7 @@ class ScoreTest(unittest.TestCase):
 
     def test_do_total_scoring_female_10(self):
         parents_score_file = "data/sample/inputdata_Conners3parent_female10.csv"
-        actual_results = do_total_scoring(parents_score_file, 10, "female")
+        actual_results = do_total_scoring(parents_score_file, 10, "female", 'parent')
         expected_results = {'LP': (10, 64), 'PR': (10, 90), 'IN': (21, 82), 'AG': (9, 81), 'HY': (12, 67),
                             'EF': (21, 90)}
         # IN (21, 82), HY (12, 67), LP (10, 64), EF (21, 90), AG (9, 81), PR (10, 90)
@@ -27,8 +27,9 @@ class ScoreTest(unittest.TestCase):
 
     def test_do_total_scoring_male_10(self):
         parents_score_file = "data/sample/inputdata_Conners3parent_male10.csv"
-        actual_results = do_total_scoring(parents_score_file, 10, "male")
-        expected_results = {'LP': (10, 64), 'IN': (19, 75), 'AG': (5, 63), 'HY': (29, 89), 'EF': (13, 61), 'PR': (0, 43)}
+        actual_results = do_total_scoring(parents_score_file, 10, "male", 'parent')
+        expected_results = \
+            {'LP': (10, 64), 'IN': (19, 75), 'AG': (5, 63), 'HY': (29, 89), 'EF': (13, 61), 'PR': (0, 43)}
         # SUB-10016-01: {'LP': (10, 64), 'IN': (19, 75), 'AG': (5, 63), 'HY': (29, 89), 'EF': (13, 61), 'PR': (0, 43)}
         expected_keys = expected_results.keys()
         self.assertEqual(len(expected_keys), len(actual_results.keys()))
@@ -37,7 +38,7 @@ class ScoreTest(unittest.TestCase):
 
     def test_get_t_score_from_raw_score_high(self):
         raw_score = 23
-        csv_file = 'data/constant/male_lp.csv'
+        csv_file = 'data/constant/parent/male_lp.csv'
         df = pd.read_csv(csv_file)
         age = 9
         age_str = str(age)
@@ -49,7 +50,7 @@ class ScoreTest(unittest.TestCase):
 
     def test_get_t_score_from_raw_score_low(self):
         raw_score = 0
-        csv_file = 'data/constant/male_lp.csv'
+        csv_file = 'data/constant/parent/male_lp.csv'
         df = pd.read_csv(csv_file)
         age = 9
         age_str = str(age)
